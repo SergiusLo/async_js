51d90a499f970e712ab1fe3dc67b996d7d6f320d
Serj <loboyko99911@gmail.com>
2022-10-11 23:05:10 +0300

    callback-hell

--- script.js
+++ script.js
@@ -4,22 +4,46 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 //////////////////////////////////////////////////////
-const getCountryData = function (countryName) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
-  request.send();
-  // console.log(request.responseText);
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    //   cons data = JSON.parse(this.responseText)[0]; - тоже что и выше только в старом формате
-    const currencies = data.currencies;
-    const currensyName = Object.values(currencies)[0].name;
-    const currensySymbol = Object.values(currencies)[0].symbol;
-    const languages = data.languages;
-    const firstLanguage = Object.values(languages)[0];
-    console.log(data);
-    const html = `
-    <article class="country">
+// const getCountryData = function (countryName) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
+//   request.send();
+//   // console.log(request.responseText);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     //   cons data = JSON.parse(this.responseText)[0]; - тоже что и выше только в старом формате
+//     const currencies = data.currencies;
+//     const currensyName = Object.values(currencies)[0].name;
+//     const currensySymbol = Object.values(currencies)[0].symbol;
+//     const languages = data.languages;
+//     const firstLanguage = Object.values(languages)[0];
+//     console.log(data);
+//     const html = `
+//     <article class="country">
+//     <img class="country__img" src="${data.flags.svg}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name.common}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👨‍👩‍👧‍👦</span>${(
+//         +data.population / 1000000
+//       ).toFixed(1)} миллионов</p>
+//       <p class="country__row"><span>🗣️</span>${firstLanguage}</p>
+//       <p class="country__row"><span>💰</span>${currensySymbol} ${currensyName}</p>
+//     </div>
+//   </article>`;
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+const displayCountry = function (data, className = '') {
+  const currencies = data.currencies;
+  const currensyName = Object.values(currencies)[0].name;
+  const currensySymbol = Object.values(currencies)[0].symbol;
+  const languages = data.languages;
+  const firstLanguage = Object.values(languages)[0];
+  console.log(data);
+  const html = `
+    <article class="country ${className}">
     <img class="country__img" src="${data.flags.svg}" />
     <div class="country__data">
       <h3 class="country__name">${data.name.common}</h3>
@@ -31,9 +55,39 @@ const getCountryData = function (countryName) {
       <p class="country__row"><span>💰</span>${currensySymbol} ${currensyName}</p>
     </div>
   </article>`;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+const getCountryAndBorderCountries = function (countryName) {
+  // Вызов AJAX для страны
+  const request1 = new XMLHttpRequest();
+  request1.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
+  request1.send();
+  // console.log(request.responseText);
+  request1.addEventListener('load', function () {
+    const [data1] = JSON.parse(this.responseText);
+    //   cons data = JSON.parse(this.responseText)[0]; - тоже что и выше только в старом формате
+    // Отображение страны
+    displayCountry(data1);
+    // Получаем соседние страны
+    const [firstNeighbour] = data1.borders;
+    if (!firstNeighbour) return;
+
+    // Вызов AJAX для получения данных о первой соседней стране
+    const request2 = new XMLHttpRequest();
+    request2.open(
+      'GET',
+      `https://restcountries.com/v3.1/alpha/${firstNeighbour}`
+    );
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      displayCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('ukraine');
+getCountryAndBorderCountries('ukraine');
+// getCountryData('ukraine');
